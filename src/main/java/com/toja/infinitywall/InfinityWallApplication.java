6e8748aa964bd6daf31df7d815bdb4b3381de7f8
Diego Machín <diego@acidtango.com>
2023-05-28 13:53:46 +0100

    Initial commits

---
+++ src/main/java/com/toja/infinitywall/InfinityWallApplication.java
@@ -0,0 +1,13 @@
+package com.toja.infinitywall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfinityWallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfinityWallApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/toja/infinitywall/InfinityWallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toja.infinitywall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfinityWallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
